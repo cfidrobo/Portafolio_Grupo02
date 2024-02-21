874baeca859d48f603463d02c7f768c6343ac4d1
Cristian Fernando Idrobo Montalvo <105193365+cfidrobo@users.noreply.github.com>
2024-02-20 23:37:36 -0500

    v1

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Cristian Idrobo/"
-})
+  base: "/Cristian Idrobo/",
+  build: {
+    outDir: 'build', // Specify the output directory for the build
+  },
+});
